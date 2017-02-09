7e390bcf12f1c07cad4a760300d1127e23524c9c
Sinigaglia Steeve <steeve.sinigaglia@laposte.net>
2017-02-09 23:27:01 +0100

    list edge

--- src/main/java/core/AdjacencyListDirectedGraph.java
+++ src/main/java/core/AdjacencyListDirectedGraph.java
@@ -1,9 +1,11 @@
 package core;
 
 import core.node.DirectedNode;
+import core.node.Edge;
 import core.node.UndirectedNode;
 import tool.GraphTools;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,15 +18,16 @@ public class AdjacencyListDirectedGraph implements IDirectedGraph {
     private int nbArcs;
     private HashMap<Integer, DirectedNode> adjacencyList;
     private int[][] weightAdjacencyMatrix;
+    private List<Edge> edgeList;
 
-    public AdjacencyListDirectedGraph(int[][] matrix){
+    public AdjacencyListDirectedGraph(int[][] matrix) {
         this.adjacencyList = new HashMap<>();
-        for(int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             this.adjacencyList.put(i, new DirectedNode(i));
         }
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix.length; j++){
-                if(matrix[i][j] == 1){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == 1) {
                     this.adjacencyList.get(i).addSuccessor(j);
                     this.adjacencyList.get(j).addPredecessor(i);
                 }
@@ -32,12 +35,12 @@ public class AdjacencyListDirectedGraph implements IDirectedGraph {
         }
         this.nbNodes = this.adjacencyList.size();
         this.nbArcs = 0;
-        for(DirectedNode node : this.adjacencyList.values()){
+        for (DirectedNode node : this.adjacencyList.values()) {
             this.nbArcs += node.getNbArcs();
         }
     }
 
-    public AdjacencyListDirectedGraph(IDirectedGraph directedGraph){
+    public AdjacencyListDirectedGraph(IDirectedGraph directedGraph) {
         this.nbArcs = directedGraph.getNbArcs();
         this.nbNodes = directedGraph.getNbNodes();
         this.adjacencyList = (HashMap<Integer, DirectedNode>) directedGraph.getAdjacencyList().clone();
@@ -90,10 +93,10 @@ public class AdjacencyListDirectedGraph implements IDirectedGraph {
     @Override
     public int[][] toAdjacencyMatrix() {
         int n = this.adjacencyList.size();
-        int[][] adjacencyMatrix = new int [n][n];
-        for(int i = 0; i < this.adjacencyList.size(); i++) {
-            for(int j = 0; j < this.adjacencyList.size(); j++) {
-                if(this.adjacencyList.get(i).hasSuccessor(j)) adjacencyMatrix[i][j] = 1;
+        int[][] adjacencyMatrix = new int[n][n];
+        for (int i = 0; i < this.adjacencyList.size(); i++) {
+            for (int j = 0; j < this.adjacencyList.size(); j++) {
+                if (this.adjacencyList.get(i).hasSuccessor(j)) adjacencyMatrix[i][j] = 1;
                 else adjacencyMatrix[i][j] = 0;
             }
         }
@@ -103,7 +106,7 @@ public class AdjacencyListDirectedGraph implements IDirectedGraph {
     @Override
     public IDirectedGraph computeInverse() {
         AdjacencyListDirectedGraph inverseGraph = new AdjacencyListDirectedGraph(this);
-        for(DirectedNode node : inverseGraph.adjacencyList.values()){
+        for (DirectedNode node : inverseGraph.adjacencyList.values()) {
             List<Integer> tmp = node.getSuccessors();
             node.setSuccessors(node.getPredecessors());
             node.setPredecessors(tmp);
@@ -111,7 +114,7 @@ public class AdjacencyListDirectedGraph implements IDirectedGraph {
         return inverseGraph;
     }
 
-    public int[][] setRandomWeights(int range, boolean acceptNegatif){
+    public int[][] setRandomWeights(int range, boolean acceptNegatif) {
         this.weightAdjacencyMatrix = GraphTools.generateGraphWeightData(this.toAdjacencyMatrix(), range, acceptNegatif);
         return this.weightAdjacencyMatrix;
     }
@@ -129,6 +132,33 @@ public class AdjacencyListDirectedGraph implements IDirectedGraph {
 
     @Override
     public void setWeightAdjacencyMatrix(int[][] matrix) {
+        int[][] adjacencyMatrix = toAdjacencyMatrix();
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            for (int j = 0; j < adjacencyMatrix.length; j++) {
+                if (adjacencyMatrix[i][j] == 0)
+                    matrix[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
         this.weightAdjacencyMatrix = matrix;
+        buildListEdge();
+
     }
+
+    @Override
+    public List<Edge> getEdges() {
+        return this.edgeList;
+    }
+
+    protected void buildListEdge() {
+        this.edgeList = new ArrayList<>();
+        for (int i = 0; i < this.weightAdjacencyMatrix.length; i++) {
+            for (int j = 0; j < this.weightAdjacencyMatrix.length; j++) {
+                if (this.weightAdjacencyMatrix[i][j] != Integer.MAX_VALUE) {
+                    this.edgeList.add(new Edge(i, j, weightAdjacencyMatrix[i][j]));
+                }
+            }
+        }
+    }
+
 }
--- src/main/java/core/IDirectedGraph.java
+++ src/main/java/core/IDirectedGraph.java
@@ -1,6 +1,7 @@
 package core;
 
 import core.node.DirectedNode;
+import core.node.Edge;
 import javafx.util.Pair;
 import sun.reflect.generics.tree.Tree;
 import tool.ArrayTools;
@@ -28,6 +29,8 @@ public interface IDirectedGraph extends IGraph {
 
     void setWeightAdjacencyMatrix(int[][] matrix);
 
+    List<Edge> getEdges();
+
     default LinkedHashMap<Integer, Integer> bellmanv2(int s) throws Exception {
         LinkedHashMap<Integer, Integer> dists = new LinkedHashMap<>();
         LinkedHashMap<Integer, Integer> preds = new LinkedHashMap<>();
@@ -45,7 +48,7 @@ public interface IDirectedGraph extends IGraph {
         for (int k = 1; k < this.getNbNodes() - 1; k++) {
             for (int x = 0; x < this.getNbNodes(); x++) {
                 int node = (int) dists.keySet().toArray()[x];
-                for (int y : this.getSuccessors(node)){
+                for (int y : this.getSuccessors(node)) {
                     int current = dists.get(y);
                     int compareTo = dists.get(x) + this.getWeight(node, y);
                     dists.put(y, GraphTools.getMinBetween(current, compareTo));
@@ -55,7 +58,7 @@ public interface IDirectedGraph extends IGraph {
 
 
         for (int v = 0; v < this.getNbNodes(); v++) {
-            for (int succ : this.getSuccessors((Integer) dists.keySet().toArray()[v])){
+            for (int succ : this.getSuccessors((Integer) dists.keySet().toArray()[v])) {
                 if (dists.get(succ) > dists.get(v) + this.getWeight(v, succ))
                     throw new Exception("Negative cycle found");
 
@@ -76,13 +79,10 @@ public interface IDirectedGraph extends IGraph {
             pred[i] = 0;
         }
         dist[s] = 0;
-        for (int k = 0; k < this.getNbNodes() - 1; k++) {
-            for (int x = 0; x < this.getNbNodes(); x++) {
-                for (int y : this.getSuccessors(x)) {
-                    int current = dist[y];
-                    int compareTo = dist[x] + this.getWeight(x, y);
-                    dist[y] = GraphTools.getMinBetween(current, compareTo);
-                }
+        for (int k = 1; k < this.getNbNodes() - 1; k++) {
+            for (Edge edge : this.getEdges()) {
+                dist[edge.to] = GraphTools.getMinBetween(dist[edge.to],
+                        dist[edge.from] + this.getWeight(edge.from, edge.to));
             }
         }
 
